5e6c0d00a2159e18a4fab1829a5ca8cbac656fe1
Maggy <edoziemagdalene@gmail.com>
2023-04-03 18:27:35 +0100

    ...

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,6 +1,26 @@
-const Banner = () =>{
-return <div>banner</div>
-}
+import styles from ""
+
+const Banner = (props) =>{
+    const {title, subTitle, imgUrl} = props;
+
+    const handleOnPlay = () =>{
+
+    }
+
+    return <div>
+            <h3>{title}</h3>
+            <h3>{subTitle}</h3>
+            <button onClick={handleOnPlay}>Play</button>
+            <div style={{
+                backgroundImage:`url(${imgUrl})`,
+                width:"100%", 
+                height:"100%",
+                position: 'absolute',
+                backgroundSize: 'cover',
+                backgroundPosition:"50%"
+                }}></div>
+            </div>
+    }
 
 
 export default Banner;
