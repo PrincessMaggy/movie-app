1d39de4d8d8ce963ad98c294393c4d85ec757bdb
Maggy <edoziemagdalene@gmail.com>
2023-04-04 22:03:20 +0100

    completed banner component

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,4 +1,6 @@
-import styles from ""
+import styles from "./banner.module.css"
+import Image from "next/image";
+
 
 const Banner = (props) =>{
     const {title, subTitle, imgUrl} = props;
@@ -7,18 +9,33 @@ const Banner = (props) =>{
 
     }
 
-    return <div>
-            <h3>{title}</h3>
-            <h3>{subTitle}</h3>
-            <button onClick={handleOnPlay}>Play</button>
-            <div style={{
+    return <div className={styles.container}>
+        <div className={styles.leftWrapper}>
+            <div className={styles.left}>
+            <div className={styles.nseriesWrapper}>
+                <p className={styles.firstLetter}>N</p>
+                <p className={styles.series} >S E R I E S</p>
+            </div>
+                <h3 className={styles.title}>{title}</h3>
+                <h3 className={styles.subTitle}>{subTitle}</h3>
+                <div className={styles.playBtnWrapper}>
+                    <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+                    <Image 
+                    src="/static/play.svg"  
+                    alt="play icon"  
+                    width="32" 
+                    height="32"
+
+                    />
+                    <span className={styles.playText}>Play</span></button>
+                </div>
+                </div>
+          </div>
+            <div className={styles.bannerImg} style={{
                 backgroundImage:`url(${imgUrl})`,
-                width:"100%", 
-                height:"100%",
-                position: 'absolute',
-                backgroundSize: 'cover',
-                backgroundPosition:"50%"
+                
                 }}></div>
+                
             </div>
     }
 
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>movie app</h1>
+      <h1>Movie app</h1>
 <Banner title="Clifford the red dog"
   subTitle ="a very cute dog"
   imgUrl="/static/clifford.webp"
